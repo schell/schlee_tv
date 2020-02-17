d3682b943ab9a61a286b078112c5ecda95f749b7
Schell Scivally <efsubenovex@gmail.com>
2020-02-17 13:17:52 -0800

    thrash ms

--- scripts/install_milkshake.sh
+++ scripts/install_milkshake.sh
@@ -3,6 +3,7 @@
 
 if hash ~/.local/bin/milkshake-cli 2>/dev/null; then
     echo "milkshake is already installed"
+    ls -lah ~/.local/bin
 else
     wget https://github.com/schell/milkshake/releases/download/v0.3.0.0/milkshake-cli.tar.gz
     tar xvfz milkshake-cli.tar.gz
