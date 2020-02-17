01275f71e6c76e00248b3c39336b62d74f413a98
Schell Scivally <efsubenovex@gmail.com>
2020-02-17 13:09:07 -0800

    milkshake-cli bin

--- scripts/install_milkshake.sh
+++ scripts/install_milkshake.sh
@@ -7,7 +7,7 @@ if hash milkshake-cli 2>/dev/null; then
 else
     wget https://github.com/schell/milkshake/releases/download/v0.3.0.0/milkshake-cli.tar.gz
     tar xvfz milkshake-cli.tar.gz
-    mv milkshake-cli /usr/local/bin
+    mv milkshake-cli ~/.local/bin
     echo "Installed milkshake into:"
     which milkshake-cli
 fi
