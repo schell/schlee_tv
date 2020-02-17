b8a7253786411871765bd3e2520128548df10f11
Schell Scivally <efsubenovex@gmail.com>
2020-02-17 13:21:53 -0800

    thrash ms

--- scripts/install_milkshake.sh
+++ scripts/install_milkshake.sh
@@ -2,12 +2,12 @@
 
 mkdir -p ~/.local/bin
 
-if hash ~/.local/bin/milkshake-cli 2>/dev/null; then
+if [ -f "~/.local/bin/milkshake-cli" ]; then
     echo "milkshake is already installed"
     ls -lah ~/.local/bin
 else
     wget https://github.com/schell/milkshake/releases/download/v0.3.0.0/milkshake-cli.tar.gz
     tar xvfz milkshake-cli.tar.gz
-    mv milkshake-cli ~/.local/bin
+    mv milkshake-cli ~/.local/bin/milkshake-cli
     echo "Installed milkshake"
 fi
