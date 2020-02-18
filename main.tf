001a4a346b504499adf37085392f3b2c4ac826bb
Schell Scivally <efsubenovex@gmail.com>
2020-02-18 09:55:18 -0800

    don't try to destroy master

--- main.tf
+++ main.tf
@@ -75,3 +75,33 @@ resource "null_resource" "aws_sync" {
     always_run = timestamp()
   }
 }
+
+
+// ssl & route53 stuff
+resource "aws_acm_certificate" "cert" {
+  domain_name = local.domain_name
+  validation_method = "DNS"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+resource "aws_route53_zone" "zone" {
+  name = local.domain_name
+}
+
+
+resource "aws_route53_record" "cert_validation" {
+  name    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_type
+  zone_id = aws_route53_zone.zone.id
+  records = [aws_acm_certificate.cert.domain_validation_options.0.resource_record_value]
+  ttl     = 60
+}
+
+
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+}
--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -2,8 +2,13 @@
 
 workspace=$1
 
+if [ "$workspace" == "master" ]; then
+    echo "Will not delete a master deployment"
+    exit 1
+fi
+
 terraform init || exit 1
 terraform workspace select $workspace || terraform workspace new $workspace
-terraform destroy --auto-approve -var="is_teardown=true"
+terraform destroy --auto-approve
 terraform workspace select default
 terraform workspace delete $workspace
