a44207c62e0efe82819982ac090843fb31ce6319
Schell Scivally <efsubenovex@gmail.com>
2020-02-27 10:08:04 -0800

    terraform is working

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # schlee_tv
 schlee.tv
+
+## learnings
+* in order for the rout53 alias record to be pointed at the cloudfront url, the
+  cloudfront distribution must have the domain name listed in its aliases, eg.
+  ```terraform
+  resource "aws_cloudfront_distribution" "distribution" {
+    ...
+    aliases = [
+      "schlee.tv"
+    ]
+    ...
+  ```
+* in order for cloudfront to be able to read from the s3 bucket we must first
+  create a cloudfront access origin id and then place a policy on the bucket that
+  allows the origin id to read the bucket's objects:
+  ```terraform
+  resource "aws_cloudfront_origin_access_identity" "origin_identity" {
+    comment = "identity for schlee.tv access origin"
+  }
+
+  resource "aws_s3_bucket" "schlee_tv_bucket" {
+    bucket = "schlee.tv"
+    acl = "private"
+
+    policy = <<POLICY
+  {
+      "Version": "2008-10-17",
+      "Id": "PolicyForCloudFrontPrivateContent",
+      "Statement": [
+          {
+              "Sid": "1",
+              "Effect": "Allow",
+              "Principal": {
+                  "AWS": "${aws_cloudfront_origin_access_identity.origin_identity.iam_arn}"
+              },
+              "Action": "s3:GetObject",
+              "Resource": "arn:aws:s3:::schlee.tv/*"
+          }
+      ]
+  }
+  POLICY
+  }
+  ```
+  ...and then set this as the aws_cloudfront_distribution.s3_origin_config.origin_access_identity:
+  ```terraform
+  resource "aws_cloudfront_distribution" "distribution" {
+    origin {
+      ...
+      s3_origin_config {
+        origin_access_identity = aws_cloudfront_origin_access_identity.origin_identity.cloudfront_access_identity_path
+      }
+    }
+    ...
+  }
+  ```
--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 // variables
 locals {
   is_master = terraform.workspace == "master"
-  domain_name = terraform.workspace == "master" ? "schlee.tv" : "${terraform.workspace}.schlee.tv"
+  //domain_name = terraform.workspace == "master" ? "schlee.tv" : "${terraform.workspace}.schlee.tv"
 }
 
 
@@ -24,36 +24,48 @@ variable "AWS_SECRET_ACCESS_KEY" {
 
 // aws
 provider "aws" {
+  region = "us-west-2"
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
-  region = "us-west-2"
+}
+
+
+provider "aws" {
+  alias = "virginia"
+  region = "us-east-1"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+}
+
+
+// origin id & cloudfront
+resource "aws_cloudfront_origin_access_identity" "origin_identity" {
+  comment = "identity for schlee.tv access origin"
 }
 
 
 // s3
 resource "aws_s3_bucket" "schlee_tv_bucket" {
-  bucket = local.domain_name
-  acl = "public-read"
+  bucket = "schlee.tv"
+  acl = "private"
 
-  policy = <<EOF
+  policy = <<POLICY
 {
-    "Version":"2012-10-17",
-    "Statement":[
-      {
-        "Sid":"AddPerm",
-        "Effect":"Allow",
-        "Principal": "*",
-        "Action":["s3:GetObject"],
-        "Resource":["arn:aws:s3:::${local.domain_name}/*"]
-      }
+    "Version": "2008-10-17",
+    "Id": "PolicyForCloudFrontPrivateContent",
+    "Statement": [
+        {
+            "Sid": "1",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "${aws_cloudfront_origin_access_identity.origin_identity.iam_arn}"
+            },
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::schlee.tv/*"
+        }
     ]
-  }
-  EOF
-
-  website {
-    index_document = "index.html"
-    error_document = "404.html"
-  }
+}
+POLICY
 
   provisioner "local-exec" {
     when = destroy
@@ -65,43 +77,118 @@ resource "aws_s3_bucket" "schlee_tv_bucket" {
 // s3 site sync
 resource "null_resource" "aws_sync" {
   provisioner "local-exec" {
-    command = <<EOF
-      aws s3 sync build/site s3://${local.domain_name}/ && \
-      echo endpoint is ${aws_s3_bucket.schlee_tv_bucket.website_endpoint}
-    EOF
+    command = "aws s3 sync build/site s3://schlee.tv/"
   }
 
   triggers = {
     always_run = timestamp()
   }
+
+  depends_on = [aws_s3_bucket.schlee_tv_bucket]
 }
 
 
-// ssl & route53 stuff
+data "aws_route53_zone" "zone" {
+  provider = aws.virginia
+  zone_id = "Z3TROWZ11JUKZU"
+}
+
+
+// ssl & route53 record stuff
 resource "aws_acm_certificate" "cert" {
-  domain_name = local.domain_name
+  provider = aws.virginia
   validation_method = "DNS"
+  domain_name = "schlee.tv"
+  tags = {
+    Name = "schlee_tv_cert"
+  }
   lifecycle {
     create_before_destroy = true
   }
 }
 
 
-resource "aws_route53_zone" "zone" {
-  name = local.domain_name
-}
-
-
 resource "aws_route53_record" "cert_validation" {
-  name    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_name
-  type    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_type
-  zone_id = aws_route53_zone.zone.id
+  provider = aws.virginia
+  zone_id = data.aws_route53_zone.zone.id
+
+  name = aws_acm_certificate.cert.domain_validation_options.0.resource_record_name
+  type = aws_acm_certificate.cert.domain_validation_options.0.resource_record_type
   records = [aws_acm_certificate.cert.domain_validation_options.0.resource_record_value]
-  ttl     = 60
+  ttl = 60
 }
 
 
-resource "aws_acm_certificate_validation" "cert" {
+resource "aws_acm_certificate_validation" "cert_validation" {
+  provider                = aws.virginia
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+  validation_record_fqdns = aws_route53_record.cert_validation.*.fqdn
+  timeouts {
+    create = "60m"
+  }
+  lifecycle {
+    ignore_changes = [id]
+  }
+}
+
+
+resource "aws_cloudfront_distribution" "distribution" {
+  origin {
+    domain_name = aws_s3_bucket.schlee_tv_bucket.bucket_regional_domain_name
+    origin_id = aws_s3_bucket.schlee_tv_bucket.id
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_identity.cloudfront_access_identity_path
+    }
+  }
+  enabled = true
+  is_ipv6_enabled = true
+  comment = "schlee.tv cloudfront"
+  default_root_object = "index.html"
+  aliases = [
+    "schlee.tv"
+  ]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.schlee_tv_bucket.id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  // Here's where our certificate is loaded in!
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate_validation.cert_validation.certificate_arn
+    ssl_support_method  = "sni-only"
+    minimum_protocol_version = "TLSv1.1_2016"
+  }
+}
+
+
+// last bit of route53 stuff
+resource "aws_route53_record" "main" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name = ""
+  type = "A"
+  alias {
+    name = aws_cloudfront_distribution.distribution.domain_name
+    zone_id = aws_cloudfront_distribution.distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
 }
