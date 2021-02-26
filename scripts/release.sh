85db33b300187a4beb359174465ccb972252499b
Schell Carl Scivally <efsubenovex@gmail.com>
2021-02-26 10:20:31 -0800

    Don't use terraform on each update.

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,6 +2,17 @@
 
 workspace=$1
 
-terraform init || exit 1
-terraform workspace select $workspace || terraform workspace new $workspace
-terraform apply --auto-approve
+#terraform init || exit 1
+#terraform workspace select $workspace || terraform workspace new $workspace
+#terraform apply --auto-approve
+
+if [$workspace == "master"]
+then 
+    domain_name="schlee.tv" 
+else 
+    domain_name="${workspace}.schlee.tv" 
+fi
+
+echo "Deploying to ${domain_name}"
+
+aws s3 sync build/site s3://${local.domain_name}/
